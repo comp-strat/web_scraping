eb87945ee209b2f49c93aac12a54c6b2cd20bec2
arjunsrinivasan1997 <arjunsrinivasan@berkeley.edu>
2017-12-06 11:42:19 -0800

    More multiprocessing tests

--- scripts/ThreadingHelloWorld.py
+++ scripts/ThreadingHelloWorld.py
@@ -1,9 +1,12 @@
 import multiprocessing
+import time
 
 
 def worker(num):
     """thread worker function"""
-    print("Hello from thread %s" % num)
+    print("Hello from thread %s (process starting)" % num)
+    time.sleep(num)
+    print("Goodbye from thread %s (process ending)" % num)
     return
 
 
