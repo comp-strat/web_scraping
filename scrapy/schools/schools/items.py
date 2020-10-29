4b26126e75cc6faf14c60c93c8d3933813634400
Andrew Chen <chen_andrew@berkeley.edu>
2020-10-29 14:53:09 -0400

    Create CharterItem item

--- scrapy/schools/schools/items.py
+++ scrapy/schools/schools/items.py
@@ -3,10 +3,10 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
-
-class SchoolsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class CharterItem(Item):
+    url_from = Field()
+    url_to = Field()
+    
+    
