9ee9736c25289cc8b5cf8ac04c62073b9161a931
Andrew Chen <chen_andrew@berkeley.edu>
2020-10-29 13:26:11 -0400

    Experiment with CrawlSpider

--- scrapy/scrapy_vanilla.py
+++ scrapy/scrapy_vanilla.py
@@ -8,7 +8,7 @@ from scrapy.spiders import Rule #, CrawlSpider
 class CharterSchoolSpider(scrapy.Spider):
     name = 'to be renamed'
     allowed_domains = ['https://ggcs.cyberschool.com/']
-    start_urls = ['http://https://ggcs.cyberschool.com//']
+    start_urls = ['http://https://ggcs.cyberschool.com/']
     rules = [
         Rule(
             LinkExtractor(
--- scrapy/sublinks/sublinks/spiders/sublinks_spider.py
+++ scrapy/sublinks/sublinks/spiders/sublinks_spider.py
@@ -5,16 +5,14 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class SublinksSpiderSpider(CrawlSpider):
     name = 'sublinks_spider'
-    allowed_domains = ['web']
-    start_urls = ['http://web/']
+    start_urls = ['https://www.socratesacademy.us']
 
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        print("-----")
+        print(response.body)
+        print("--END--")
+        return response.body
