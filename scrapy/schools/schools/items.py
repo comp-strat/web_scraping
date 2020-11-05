949bf5f65a3eedc2a3d45b9566a94420654a7734
Andrew Chen <chen_andrew@berkeley.edu>
2020-11-05 11:19:41 -1000

    Add more item fields

--- scrapy/schools/schools/items.py
+++ scrapy/schools/schools/items.py
@@ -7,11 +7,16 @@ from scrapy.item import Item, Field
 
 class CharterItem(Item):
     """
-    The url of the item, the text with tags stripped,
-    and the depth as defined by DepthMiddleware. 
+    Represents the object parsed by the spider.
+    Depth is defined by DepthMiddleware. 
     """
-    url = Field()
-    text = Field()
-    depth = Field()
-    
-    
+    url = Field() # str
+    text = Field() # str   
+    depth = Field() # int
+    is_pdf = Field() # bool
+    is_doc = Field() # bool
+    is_im = Field() # bool
+    status = Field() # int
+    id = Field() # int this is school id or ncessch
+    timestamp = Field() # datetime
+    date_archived = Field() # datetime
