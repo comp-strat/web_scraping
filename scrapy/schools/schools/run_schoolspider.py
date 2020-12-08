f1787dc36f532d593ce3e6d0d1dc8371e463a9f2
Andrew Chen <andrewchen747@gmail.com>
2020-12-08 00:26:52 -0800

    Fix requirements and create .dockerignore

--- scrapy/schools/schools/run_schoolspider.py
+++ scrapy/schools/schools/run_schoolspider.py
@@ -1,10 +1,14 @@
 """
-By running this file, the schoolspider is run.
-Used in the Scrapy Dockerfile.
+In scrapy/schools, use the command
+
+    python3 schools/run_schoolspider.py 
+
+To run the schoolspider.
+The primary purpose of this file is for the Scrapy Dockerfile.
 """
 from scrapy import cmdline
 
 # See scrapy_vanilla.py for the meaning of this command.
-scrapy_run_cmd = "scrapy crawl schoolspider -a csv_input=spiders/test_urls.csv -o schoolspider_output.json"
+scrapy_run_cmd = "scrapy crawl schoolspider -a csv_input=schools/spiders/test_urls.csv -o schools/schoolspider_output.json"
 
 cmdline.execute(scrapy_run_cmd.split())
