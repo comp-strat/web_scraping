f9e70541606644c63991fd97374f1d8cdd95d0ca
pmasaya <pmasaya@berkeley.edu>
2021-04-13 19:36:54 -0700

    implement basic school string in website text check

--- url_scraping/checking_URLs.py
+++ url_scraping/checking_URLs.py
@@ -41,12 +41,11 @@ def dict_to_csv(dictionary, file_name, header):
             
         dict_writer.writerow(dictionary)
 
-def check_schoolstr_website(url):
+def check_schoolstr_website(school_name, url):
     source = requests.get(url)
 
     soup = BeautifulSoup(source.text, 'html.parser')
-    print(soup.text)
-    return True
+    return school_name in soup.text
 
 ### Main Function
 
@@ -66,17 +65,15 @@ with open(input_file, 'r', encoding = 'utf-8') as csvfile:
     url_confirmations = []
     i = 1
     for row in reader: # loop through rows in input file
-        #schoolstr_in_website = check_schoolstr_website(row["URL"]) # Check if the entry's string can be found in the website found. If not, return False.
+        schoolstr_in_website = check_schoolstr_website(row["SCH_NAME"], row["URL"]) # Check if the entry's string can be found in the website found. If not, return False.
 
         if list(row.values()) == ["", "", "", "", ""]:
             print("End of file")
             break
 
         elif old_exists and row["SCH_NAME"] in list(old_output.SCH_NAME):
-            the_input = pd.read_csv(input_file)
-
-            if 'validity_confirmed' in list(the_input.columns):
-                url_confirmations += [row['validity_confirmed']] # take old value for 'validity_confirmed'
+            if 'validity_confirmed' in list(row.keys()):
+                url_confirmations += [bool(row['validity_confirmed'])] # take old value for 'validity_confirmed'
             else:
                 url_confirmations += []
             pass
@@ -85,14 +82,16 @@ with open(input_file, 'r', encoding = 'utf-8') as csvfile:
             print("Query Ranking > 5")
             url_confirmations += [False]
 
-        #elif not schoolstr_in_website:
-        #    print("School String not found in website")
+        elif not schoolstr_in_website:
+            print("School String not found in website")
+            url_confirmations += [False]
 
         else: # Add new row to 'output_file'
             print("confirming validity of school's URL!")
             dict_to_csv(row, output_file, list(row.keys()))
             url_confirmations += [True]
 
+print(url_confirmations)
 df['validity_confirmed'] = url_confirmations
 df.to_csv(input_file, index=False) # update 'input_file' with new values for "validity_confirmed"
 
