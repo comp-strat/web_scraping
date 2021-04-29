fdabfa7b7e04e21af1fcbb8dc7aa1c643c27da8c
Ubuntu <azureuser@gss-mdi-vm.img05b25w1ue1e3flkjdqnqhtg.cx.internal.cloudapp.net>
2021-04-29 15:09:30 +0000

    added try-catch "resiliency" to multi-spider run

--- scrapy/schools/schools/execute_scrapy_from_file.py
+++ scrapy/schools/schools/execute_scrapy_from_file.py
@@ -5,5 +5,9 @@ SCRAPY_RUN_CMD = "scrapy crawl schoolspider -a school_list="
 def execute_scrapy_from_file(filename):
     run_cmd = SCRAPY_RUN_CMD + str(filename)
     print(run_cmd)
-    return cmdline.execute(run_cmd.split())
+    try:
+        cmdline.execute(run_cmd.split())
+        return 0
+    except:
+        return 1
 
