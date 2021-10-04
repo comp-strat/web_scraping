05a226743217f2ec14da4e32444b1bdc39c50665
Dev Bali <devbali02@gmail.com>
2021-10-03 20:03:26 -0700

    Initial Travis Test

---
+++ .build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+python3 -m venv .venv
+source .venv/bin/activate
+cd scrapy/schools
+pip3 install -r requirements.txt
+
+#wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+#apt update && apt install -y mongodb-org
+#apt install apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#apt-cache policy docker-ce
+#apt install docker-ce
+
+apt install redis-server
+
+docker pull mongo && docker run --name mongodb -e MONGO_INITDB_ROOT_USERNAME=admin -r MONGO_INITDB_ROOT_PASSWORD=admin -p 27017:27017 mongo
+rq worker crawling-tasks &
+python schools/app.py
