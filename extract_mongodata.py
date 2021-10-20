bf25603f73108106c6e9c79277c4a39687cbe4b7
yingliu1206 <71619071+yingliu1206@users.noreply.github.com>
2021-10-20 17:21:51 -0400

    Update extract_mongodata.py

--- extract_mongodata.py
+++ extract_mongodata.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import pymongo
+import gridfs
+
 
 
 if __name__ == '__main__':
-    client = pymongo.MongoClient('mongodb://localhost:27000',
+    client = pymongo.MongoClient('mongodb://localhost:27017',
                                  username= 'admin',
                                  password='mdipass'
                                 ) 
     db = client["schoolSpider"]
     cols =db.list_collection_names()
+    fs = gridfs.GridFS(db)
     print(cols)
+    
+    # for text data
     for coll in cols:
         if coll == 'text':
             print("collection name is : ", coll)
@@ -18,5 +21,13 @@ if __name__ == '__main__':
             cursor = collection.find({})
             for document in cursor:
                 print(document)
-        else:
-            list(db.coll.find())
+    
+    # for images and files (not completed yet)    
+        elif coll == 'images.files' or coll == 'images.chunks':
+            collection = db[coll]
+            for grid_out in collection.find({},no_cursor_timeout=True):
+                data = grid_out.read()
+            list(db.images.chunks.find())
+        
+        gridout = fs.get_last_version("tr?id=227916355119461&ev=PageView&noscript=1")
+        gridout.read()
